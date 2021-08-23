9da2147eb71ad1bb7aa457a94d2fc41e716616be
Yurij M <Yurij2015@users.noreply.github.com>
2021-08-23 22:09:36 +0300

    set deploy

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// файл vue.config.js должен быть расположен в корневом каталоге проекта
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/portfolio/'
+        : '/'
+}
