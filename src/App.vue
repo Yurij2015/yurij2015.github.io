8d97f226e3f2ef8a18e8e6df49450c032e501b48
Yurij M <Yurij2015@users.noreply.github.com>
2021-08-24 08:09:03 +0300

    test depl

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Yurij9 to Your Vue.js App"/>
+  <HelloWorld msg="Welcome Yurij19 to Your Vue.js App"/>
 </template>
 
 <script>
