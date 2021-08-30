6a05ab7875d2e7de88b5319fc6652d4d7aa11143
Yurij M <Yurij2015@users.noreply.github.com>
2021-08-30 16:00:20 +0300

    set git pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/yurij2015.github.io/'
+        : '/'
+    // https://github.com/Yurij2015/yurij2015.github.io
     // chainWebpack: config => {
     //     config
     //         .plugin('html')
