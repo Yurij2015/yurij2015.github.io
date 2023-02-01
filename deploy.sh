df95809c843992ef111d37e969586f84c16b55d4
Yurii Mokryi <mokruy9999@gmail.com>
2023-02-01 23:21:28 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,5 +24,7 @@ git push -f git@github.com:Yurij2015/yurij2015.github.io.git master
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # git push -f git@github.com:Yurij2015/yurij2015.github.io.git master:gh-pages
 
+# if error "npm run build" in Node.js v18.13.0 - run command - export NODE_OPTIONS=--openssl-legacy-provider
+
 
 cd -
