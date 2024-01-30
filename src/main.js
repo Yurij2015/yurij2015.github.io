76a3d06a450153920cbf0545ce761af75becc035
Yurii Mokryi <mokruy9999@gmail.com>
2024-01-30 20:14:39 +0100

    pf(imp), change template, MP-1

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './scss/style.scss'
+import {useData} from "./composables/data.js"
+import {useLanguage} from "./composables/language.js"
+import {useNavigation} from "./composables/navigation.js"
+import {createAppRouter} from "./router/router.js"
+import {createApp} from "vue"
+import App from './vue/core/App.vue'
+
+const data = useData()
+
+data.fetchEssentials().then(r => {
+    const language = useLanguage()
+    language.init(data.getSettings()['supportedLanguages'])
+
+    const navigation = useNavigation()
+    navigation.init(data.getSections(), data.getCategories())
+
+    const router = createAppRouter()
+    const app = createApp(App)
+    app.use(router).mount('#app')
+    app.config.devtools = true;
+})
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from "axios"
-
-// axios.defaults.baseURL = 'http://localhost:8000/api/'
-axios.defaults.baseURL = 'https://stud.vv.si/api/'
-createApp(App).use(router).mount('#app')
